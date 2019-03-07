df0c6167568b645a9b93639c282af362da91cc44
superiorSilicon <ssilicon@null.net>
2019-03-07 22:58:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ photoAjax.open('get', './currentprofilepic.json');
 photoAjax.onreadystatechange = function() {
     if (photoAjax.status == 200 && photoAjax.readyState == 4) {
         var url = JSON.parse(photoAjax.responseText).url;
-        document.getElementById('useravatar').setAttribute('src', url)
+        document.getElementById('useravatar').setAttribute('src', url);
+        document.getElementById('favicon').setAttribute('href', url);
     }
+}
+photoAjax.onerror = function(err) {
+    console.log(error)
 }
 photoAjax.send();
