42a34861ff4dbc44c98c5b6b90da12fdb3fd74b3
superiorSilicon <ssilicon@null.net>
2019-03-08 12:55:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ photoAjax.onerror = function(err) {
     console.log(error)
 }
 photoAjax.send();
+
+var screenshots = document.querySelectorAll('.screenshot');
+var screenshotModal = new tingle.modal({
+    onClose: function() {
+        screenshotModal.setContent('');
+    }
+});
+
+for (var i = 0; i < screenshots.length; i++) {
+    var screenshotElement = screenshots[i];
+    var screenshotURL = screenshotElement.dataset.url;
+    screenshotElement.addEventListener('click', function(e) {
+        screenshotModal.setContent('<img class="screenshot-img" src="' + screenshotURL + '">');
+        screenshotModal.open();
+    });
+}
